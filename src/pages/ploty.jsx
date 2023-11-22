e479d0dd0bdf15efa5a29508dc1233bf7451d9db
ferdinandLZ <zhanglei00333@gmail.com>
2023-11-22 10:16:54 +0100

    202311221016

---
+++ src/pages/ploty.jsx
@@ -0,0 +1,17 @@
+import { Helmet } from 'react-helmet-async';
+
+// import { ploty } from 'src/sections/ploty/';
+
+// ----------------------------------------------------------------------
+
+export default function Ploty() {
+  return (
+    <>
+      <Helmet>
+        <title> Ploty </title>
+      </Helmet>
+
+      {/* <ploty /> */}
+    </>
+  );
+}
